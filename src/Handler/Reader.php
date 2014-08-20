3a4d4abb2f2aeef00bb5a87133202ac08a713e47
Frank de Jonge <info@frenky.net>
2014-08-20 14:27:12 +0200

    Use streams instead of strings for the response when reading.

--- src/Handler/Reader.php
+++ src/Handler/Reader.php
@@ -2,10 +2,11 @@
 
 namespace h4cc\StackFlysystem\Handler;
 
+use DateTime;
 use h4cc\StackFlysystem\HandlerInterface;
 use League\Flysystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class Reader implements HandlerInterface
 {
@@ -26,11 +27,16 @@ class Reader implements HandlerInterface
         /** @var \League\Flysystem\File $file */
         $file = $this->filesystem->get($request->getRequestUri());
 
-        $response = new Response($file->read(), 200);
+        $response = new StreamedResponse(function () use ($file) {
+            $stream = $file->readStream();
+            fpassthru($stream);
+        }, 200);
+
         $response->headers->set('Content-Type', $file->getMimetype());
         $timestamp = $file->getTimestamp();
+
         if ($timestamp) {
-            $response->setLastModified(new \DateTime('@' . $timestamp));
+            $response->setLastModified(new DateTime('@' . $timestamp));
         }
 
         return $response;
